b2f7e9b0414a0b943b77e06bc77ed74c2cfe18d5
tan <haitanle88@gmail.com>
2019-09-17 16:24:56 -0400

    add build gradle and head fragment

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,21 +16,24 @@
 
 package com.example.android.android_me.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.android_me.R;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
 
-    // TODO (1) Create a layout file that displays one body part image named fragment_body_part.xml
+    // done (1) Create a layout file that displays one body part image named fragment_body_part.xml
         // This layout should contain a single ImageView
 
-    // TODO (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
+
+    // done (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
         // In this class, you'll need to implement an empty constructor and the onCreateView method
-        // TODO (3) Show the first image in the list of head images
+        // done (3) Show the first image in the list of head images
             // Soon, you'll update this image display code to show any image you want
+
 
 
 
@@ -39,6 +42,15 @@ public class AndroidMeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
 
-        // TODO (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        // done (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        BodyPartFragment headFragment = new BodyPartFragment();
+
+        // Use a FragmentManager and transaction to add the fragment to the screen
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Fragment transaction
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, headFragment)
+                .commit();
     }
 }
